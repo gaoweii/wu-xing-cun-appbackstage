367a425b260ffa3b0949ca2aba9d3673c6a40549
gaoweii <2592006268@qq.com>
2020-02-16 20:19:37 +0800

    modify

---
+++ src/main/java/com/wuxingcunappbackstage/wuxingcunappbackstage/WuXingCunAppbackstageApplication.java
@@ -0,0 +1,13 @@
+package com.wuxingcunappbackstage.wuxingcunappbackstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuXingCunAppbackstageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WuXingCunAppbackstageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wuxingcunappbackstage/wuxingcunappbackstage/WuXingCunAppbackstageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuxingcunappbackstage.wuxingcunappbackstage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuXingCunAppbackstageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
